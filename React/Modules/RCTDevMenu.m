ff2c63977d68c4e2cac74a7dccf93a1cea75b5b8
Vojta Bartos <me@vojtechbartos.cz>
2016-11-29 13:29:29 -0800

    fixing shake to open Dev Menu only work once
    Summary:
    Fixing issues https://github.com/facebook/react-native/issues/10588
    
    This bug has been introduced during refactoring UIActionSheet to UIAlertController https://github.com/facebook/react-native/commit/d368ebfab2c389cc093f6a73e5c1021408cc6a73#diff-f78a84ff95f2bcee5a70d3accb504528
    
    **Test plan**
    
    - shake to open DevMenu, choose random menu item, it should close DevMenu and after another shaking it should open again
    Closes https://github.com/facebook/react-native/pull/10676
    
    Differential Revision: D4168254
    
    Pulled By: javache
    
    fbshipit-source-id: 6e2935b765053a2b55809af357d2b6ea03e04e8b

--- React/Modules/RCTDevMenu.m
+++ React/Modules/RCTDevMenu.m
@@ -49,8 +49,6 @@ typedef NS_ENUM(NSInteger, RCTDevMenuType) {
 
 @property (nonatomic, assign, readonly) RCTDevMenuType type;
 @property (nonatomic, copy, readonly) NSString *key;
-@property (nonatomic, copy, readonly) NSString *title;
-@property (nonatomic, copy, readonly) NSString *selectedTitle;
 @property (nonatomic, copy) id value;
 
 @end
@@ -58,6 +56,9 @@ typedef NS_ENUM(NSInteger, RCTDevMenuType) {
 @implementation RCTDevMenuItem
 {
   id _handler; // block
+
+  NSString *_title;
+  NSString *_selectedTitle;
 }
 
 - (instancetype)initWithType:(RCTDevMenuType)type
@@ -75,6 +76,15 @@ typedef NS_ENUM(NSInteger, RCTDevMenuType) {
     _value = nil;
   }
   return self;
+}
+
+- (NSString *)title
+{
+  if (_type == RCTDevMenuTypeToggle && [_value boolValue]) {
+    return _selectedTitle;
+  }
+
+  return _title;
 }
 
 RCT_NOT_IMPLEMENTED(- (instancetype)init)
@@ -121,6 +131,8 @@ RCT_NOT_IMPLEMENTED(- (instancetype)init)
 
 @end
 
+typedef void(^RCTDevMenuAlertActionHandler)(UIAlertAction *action);
+
 @interface RCTDevMenu () <RCTBridgeModule, RCTInvalidating, RCTWebSocketProxyDelegate>
 
 @property (nonatomic, strong) Class executorClass;
@@ -135,7 +147,6 @@ RCT_NOT_IMPLEMENTED(- (instancetype)init)
   NSURLSessionDataTask *_updateTask;
   NSURL *_liveReloadURL;
   BOOL _jsLoaded;
-  NSArray<RCTDevMenuItem *> *_presentedItems;
   NSMutableArray<RCTDevMenuItem *> *_extraMenuItems;
   NSString *_webSocketExecutorName;
   NSString *_executorOverride;
@@ -548,37 +559,40 @@ RCT_EXPORT_METHOD(show)
 
   NSArray<RCTDevMenuItem *> *items = [self menuItems];
   for (RCTDevMenuItem *item in items) {
-    switch (item.type) {
-      case RCTDevMenuTypeButton: {
-        [_actionSheet addAction:[UIAlertAction actionWithTitle:item.title
-                                                         style:UIAlertActionStyleDefault
-                                                       handler:^(__unused UIAlertAction *action) {
-                                                         [item callHandler];
-                                                       }]];
-        break;
-      }
-      case RCTDevMenuTypeToggle: {
-        BOOL selected = [item.value boolValue];
-        [_actionSheet addAction:[UIAlertAction actionWithTitle:(selected? item.selectedTitle : item.title)
-                                                         style:UIAlertActionStyleDefault
-                                                       handler:^(__unused UIAlertAction *action) {
-                                                         BOOL value = [self->_settings[item.key] boolValue];
-                                                         [self updateSetting:item.key value:@(!value)]; // will call handler
-                                                       }]];
-        break;
-      }
-    }
+    [_actionSheet addAction:[UIAlertAction actionWithTitle:item.title
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:[self alertActionHandlerForDevItem:item]]];
   }
 
   [_actionSheet addAction:[UIAlertAction actionWithTitle:@"Cancel"
                                                    style:UIAlertActionStyleCancel
-                                                 handler:nil]];
+                                                 handler:[self alertActionHandlerForDevItem:nil]]];
 
   _presentedItems = items;
   [RCTPresentedViewController() presentViewController:_actionSheet animated:YES completion:nil];
 }
 
+- (RCTDevMenuAlertActionHandler)alertActionHandlerForDevItem:(RCTDevMenuItem *__nullable)item
+{
+  return ^(__unused UIAlertAction *action) {
+    if (item) {
+      switch (item.type) {
+        case RCTDevMenuTypeButton: {
+          [item callHandler];
+          break;
+        }
 
+        case RCTDevMenuTypeToggle: {
+          BOOL value = [self->_settings[item.key] boolValue];
+          [self updateSetting:item.key value:@(!value)]; // will call handler
+          break;
+        }
+      }
+    }
+
+    self->_actionSheet = nil;
+  };
+}
 
 RCT_EXPORT_METHOD(reload)
 {
@@ -724,6 +738,11 @@ RCT_EXPORT_METHOD(setHotLoadingEnabled:(BOOL)enabled)
   [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentViewController:alert animated:YES completion:nil]; // Present VC
 }
 
+- (BOOL)isActionSheetShown
+{
+  return _actionSheet != nil;
+}
+
 @end
 
 #else // Unavailable when not in dev mode
@@ -734,6 +753,7 @@ RCT_EXPORT_METHOD(setHotLoadingEnabled:(BOOL)enabled)
 - (void)reload {}
 - (void)addItem:(NSString *)title handler:(dispatch_block_t)handler {}
 - (void)addItem:(RCTDevMenu *)item {}
+- (BOOL)isActionSheetShown { return NO; }
 
 @end
 
